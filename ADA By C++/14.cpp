b0e18b24d98e85de39c2b4fe86fca58454884441
Soul52913 <bingyi92@gmail.com>
2022-10-20 04:42:36 +0800

    Initial commit

--- ADA By C++/14.cpp
+++ ADA By C++/14.cpp
@@ -2,39 +2,50 @@
 #include <vector>
 
 using namespace std;
+long long minDiscomfort {};
 
 void Discomfort (vector <long long> &sleep, vector <vector <long long> > &discomfortValue, int N){
-    vector <vector <int> > minSequenceEnd;
+    vector <vector <int> > maxToEnd;
     for (int i = 0; i < N; ++i) {
         vector<int> vec;
         for (int j = 0; j < N ; ++j) {
             vec.push_back(0);
         }
-        minSequenceEnd.push_back(vec);
+        maxToEnd.push_back(vec);
     }
     for (int i = 0; i < N; ++i){
         discomfortValue[i][i] = sleep[i];
-        minSequenceEnd[i][i] = i;
+        maxToEnd[i][i] = sleep[i];
     }
     for (int l = 2; l <= N; ++l){
         for (int i = 0; i <= N - l; ++i){
             int j = i + l - 1;
-            long long combineSum {INT64_MAX};
-            if (minSequenceEnd[i][j - 1] == (j - 1)) combineSum = discomfortValue[i][j - 1] + sleep[j];
-            discomfortValue[i][j] = min (combineSum, min (discomfortValue[i][j - 1], sleep[j]));
-            if (((minSequenceEnd[i][j - 1] == (j - 1)) && (discomfortValue[i][j] == combineSum)) || (discomfortValue[i][j] == sleep[j])){
-                minSequenceEnd[i][j] = j;
+            long long combineSum = maxToEnd[i][j - 1] + sleep[j];
+            discomfortValue[i][j] = max (combineSum, max (discomfortValue[i][j - 1], sleep[j]));
+            if ((discomfortValue[i][j] == combineSum) || (discomfortValue[i][j] == sleep[j])){
+                maxToEnd[i][j] = discomfortValue[i][j];
+            }
+            else {
+                maxToEnd[i][j] = combineSum;
             }
         }
     }
     return;
 }
 
-long long MinDiscomfort (vector <long long> &sleep, vector <vector <long long> > &discomfortValue){
-    long long minDiscomfort {};
-    return minDiscomfort;
+void MinDiscomfort (vector <vector <long long> > &discomfortValue, vector <vector <long long> > &Nightmares, vector <vector <int> > &Check, int Cut, int N, int start){
+    if (Cut == 0) return;
+    for (int i = start; i < N - 1 - Cut; ++i){
+        int length = (i - 0) + 1;
+        if (Check[Cut - 1][i + 1] == 0){
+            MinDiscomfort (discomfortValue, Nightmares, Check, Cut - 1, N, i + 1);
+        }
+        Nightmares[Cut][i] =  discomfortValue[start][i] * length + Nightmares[Cut - 1][i + 1];
+        Check[Cut][i] = 1;
+    }
+    return;
 }
-
+//
 int main(){
     int N {}, K {};
     cin >> N >> K;
@@ -52,11 +63,38 @@ int main(){
         discomfortValue.push_back(vec);
     }
     Discomfort (sleep, discomfortValue, N);
-    for (int i = 0; i < N; ++i){
-        for (int j = 0; j < N; ++j){
-            cout << discomfortValue[i][j] << ' ';
+    vector <vector <long long> > Nightmares;
+    for (int i = 0; i < K; ++i) {
+        vector<long long> vec;
+        for (int j = 0; j < N - 1; ++j) {
+            vec.push_back(0);
+        }
+        Nightmares.push_back(vec);
+    }
+    vector <vector <int> > Check;
+    for (int i = 0; i < K; ++i) {
+        vector<int> vec;
+        for (int j = 0; j < N - 1; ++j) {
+            vec.push_back(0);
+        }
+        Check.push_back(vec);
+    }
+    for (int j = 0; j < N - 1; ++j) {
+        Check[0][j] = 1;
+        int length = ((N - 1) - j) + 1;
+        Nightmares[0][j] = discomfortValue[j][N - 1] * length;
+    }
+    MinDiscomfort (discomfortValue, Nightmares, Check, K - 1, N, 0);
+    long long ans = INT64_MAX;
+    for (int i = 0; i < N - 1 - (K - 1); ++i){
+        if (Nightmares[K - 1][i] < ans) ans = Nightmares[K - 1][i];
+    }
+    //cout << ans;
+    for (int i = 0; i < K; ++i){
+        for (int j = 0; j < N - 1; ++j){
+            cout << Nightmares[i][j] << ' ';
         }
         cout << '\n';
-    }
+    } 
     return 0;
 }
