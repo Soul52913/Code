e206b44fbbc75a2490e59dfe398e54ff435e3c76
Soul52913 <bingyi92@gmail.com>
2022-11-17 11:20:06 +0800

    a

---
+++ ADA By C++/4.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+vector <vector <int>>A, B;
+vector <long long> seq;
+vector <int> processed;
+vector <bool> traversed;
+
+int pN {};
+long long total {}, answer {};
+
+void dfs (int p) {
+    traversed[p] = true;
+    for (int i = 0; i < A[p].size(); ++i) {
+        if(!traversed[A[p][i]]) {
+            dfs(A[p][i]);
+        }
+    }
+    processed.push_back(p);
+    return;
+}
+void reverseDfs (int p, long long  pp) {
+    traversed[p] = true;
+    seq[p] = pp;
+    for (int i = 0; i < B[p].size(); ++i){
+        if(!traversed[B[p][i]]) {
+            reverseDfs(B[p][i], pp);
+        }
+    }
+    return;
+}
+long long kosaraju () {
+    for (int i = 0; i < pN; ++i) {
+        if (!traversed[i]) {
+            dfs(i);
+        }
+    }
+    for (int i = 0; i <= pN; ++i) traversed[i] = false;
+    long long pp {};
+    for(int i = processed.size() - 1; i >= 0; --i) {
+        if (!traversed[seq[i]]) {
+            reverseDfs(seq[i], ++pp);
+        }
+    }
+    return pp;
+}
+
+int main() {
+    int readIn {};
+    cin >> pN >> readIn;
+    vector <int> temp(2, 0);
+    vector <int> tempp;
+    for(int i = 0; i <= pN; ++i){
+        A.push_back(tempp);
+        B.push_back(tempp);
+    }
+    for(int i = 0; i < readIn; ++i){
+        cin >> temp[0] >> temp[1];
+        A[temp[0]].push_back(temp[1]);
+        B[temp[1]].push_back(temp[0]);
+    }
+    traversed.resize(pN + 1);
+    seq.resize(pN + 1);
+    answer = kosaraju();
+    cout << answer;
+    return 0;
+}
--- SP/HW2-Champion/player.c
+++ SP/HW2-Champion/player.c
@@ -1,4 +1,4 @@
-//#include "status.h"
+#include "status.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,57 +7,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-typedef enum {
-    FIRE,
-    GRASS,
-    WATER
-} Attribute;
-
-typedef struct {
-    int real_player_id; // ID of the Real Player, an integer from 0 - 7
-    int HP; // Healthy Point
-    int ATK; // Attack Power
-    Attribute attr; // Player's attribute
-    char current_battle_id; //current battle ID
-    int battle_ended_flag; // a flag for validate if the battle is the last round or not, 1 for yes, 0 for no
-} Status;
-
 #define PLAYER_PATH "./player_status.txt"
 
-int fifoFd, log_fd;
-FILE * file_fd;
+int fifoFd;
+FILE * file_fd, log_fd;
 int fullHp, agentCheck;
 Status info;
-
-void logTo(char *after, char *selfId, char *selfPid, char *targetId, char *targetPid,
-				 char *realPlayerId, char *hp, char *currentBattleId, char *endFlag){
-	memset(after, 0, strlen(after));
-	strcat(after, selfId); strcat(after, ","); strcat(after, selfPid); strcat(after, " pipe to "); strcat(after, targetId); strcat(after, ","); strcat(after, targetPid); strcat(after, " ");
-	strcat(after, realPlayerId); strcat(after, ","); strcat(after, hp); strcat(after, ","); strcat(after, currentBattleId); strcat(after, ","); strcat(after, endFlag); strcat(after, "\n");
-	return;
-}
-
-void logFrom(char *after, char *selfId, char *selfPid, char *targetId, char *targetPid,
-				 char *realPlayerId, char *hp, char *currentBattleId, char *endFlag){
-	memset(after, 0, strlen(after));
-	strcat(after, selfId); strcat(after, ","); strcat(after, selfPid); strcat(after, " pipe from "); strcat(after, targetId); strcat(after, ","); strcat(after, targetPid); strcat(after, " ");
-	strcat(after, realPlayerId); strcat(after, ","); strcat(after, hp); strcat(after, ","); strcat(after, currentBattleId); strcat(after, ","); strcat(after, endFlag); strcat(after, "\n");
-	return;
-}
-
-void fifoTo(char *after, char *selfId, char *selfPid, char *targetId, char *realPlayerId, char *hp){
-	memset(after, 0, strlen(after));
-	strcat(after, selfId); strcat(after, ","); strcat(after, selfPid); strcat(after, " fifo to "); strcat(after, targetId); strcat(after, " ");
-	strcat(after, realPlayerId); strcat(after, ","); strcat(after, hp); strcat(after, "\n");
-	return;
-}
-
-void fifoFrom(char *after, char *selfId, char *selfPid, char *targetId, char *realPlayerId, char *hp){
-	memset(after, 0, strlen(after));
-	strcat(after, selfId); strcat(after, ","); strcat(after, selfPid); strcat(after, " fifo from "); strcat(after, targetId); strcat(after, " ");
-	strcat(after, realPlayerId); strcat(after, ","); strcat(after, hp); strcat(after, "\n");
-	return;
-}
+char buf[512];
 
 int main (int argc, char **argv) {
 	int playerId = atoi(argv[1]);
@@ -65,39 +21,56 @@ int main (int argc, char **argv) {
 	pid_t pid = getpid();
 	if (playerId > 7){
 		agentCheck = 1;
-		char fifoName[256]; 
-		char *fileName = "player";
-		memset(fifoName, 0, strlen(fifoName));
-		fifoName[0] = '.', fifoName[1] = '/', fifoName[2] = '\0';
-		strcat(fifoName, fileName);
-		strcat(fifoName, argv[1]);
-		fifoFd = open(fifoName, O_RDONLY);
+		sprintf(buf, "player%d.fifo", playerId)
+		fifoFd = open(buf, O_RDONLY);
 		read(fifoFd, &info, sizeof(fifoFd));
-		info.real_player_id = playerId - 8;
 	}
 	else if (playerId <= 7 && playerId >= 0){
 		file_fd = fopen(PLAYER_PATH, "r");
 		for(int i = 0; i < playerId; ++i){
-			fscanf(file_fd, "%d%d%s%c%d", info.HP, info.ATK, info.attr, info.current_battle_id, info.battle_ended_flag);
+			char attrTemp[16];
+			fscanf(file_fd, "%d %d %s %c %d\n", &info.HP, &info.ATK, attrTemp, &info.current_battle_id, &info.battle_ended_flag);
 		}
-		info.real_player_id = playerId;
+		info.attr = (Attribute)attrTemp;
 	}
 	fullHp = info.HP;
-	char name[256], logLine[256], infoData[256], sPid[256], sRealId[256], sHp[256], sCurBattle[256], sEndFlag[256];
-	char *fileName = "log_player";
-	sprintf(sRealId, "%d", info.real_player_id);
-	memset(sRealId, 0, strlen(sRealId));
-	memset(name, 0, strlen(name));
-	name[0] = '.', name[1] = '/', name[2] = '\0';
-	strcat(name, fileName);
-	strcat(name, sRealId);
-	log_fd = open(name, O_WRONLY | O_CREAT | O_APPEND);
+	sprintf(buf, "log_player%d.txt", info.real_player_id);
+	log_fd = fopen(buf, "a+");
 	while (info.HP > 0) {
 		while (info.battle_ended_flag != 1){
-			memset(logLine, 0, strlen(logLine));
-			memset(sPid, 0, strlen(sPid)); sprintf(sPid, "%d", pid);
-			memset(sRealId, 0, strlen(sRealId)); sprintf(sRealId, "%d", info.real_player_id);
-			memset(sHp, 0, strlen(sHp)); sprintf(sHp, "%d", info.HP);
+			fprintf(log_fd, "%d,%d pipe to %c,%d %d,%d,%c,%d\n", playerId, pid, info.current_battle_id, pPid, info.real_player_id, info.HP, info.current_battle_id, info.battle_ended_flag)
+			write(1, &info, sizeof(Status));
+			read(0, &info, sizeof(Status));
+			fprintf(log_fd, "%d,%d pipe from %c,%d %d,%d,%c,%d\n", playerId, pid, info.current_battle_id, pPid, info.real_player_id, info.HP, info.current_battle_id, info.battle_ended_flag)
+		}
+		info.HP += ((fullHp - info.HP) / 2);
+		info.battle_ended_flag = 0;
+	}
+	if (agentCheck == 0) {
+		info.HP = fullHp;
+		info.battle_ended_flag = 0;
+		char fifoName[256]; 
+		char *fileName = "player";
+		char fifoId[256]; sprintf(fifoId, "%d", info.real_player_id + 7);
+		memset(fifoName, 0, strlen(fifoName));
+		fifoName[0] = '.', fifoName[1] = '/', fifoName[2] = '\0';
+		strcat(name, fileName);
+		strcat(name, fifoId);
+		mkfifo(fifoName, 0777);
+		fifoFd = open(fifoName, O_WRONLY);
+		memset(logLine, 0, strlen(logLine));
+		char sPid[256]; sprintf(sPid, "%d", pid);
+		char sTargetId[256]; sprintf(sTargetId, "%d", info.real_player_id + 7);
+		char sRealId[256]; sprintf(sRealId, "%d", info.real_player_id);
+		char sHp[256]; sprintf(sHp, "%d", info.HP);
+		fifoTo(logLine, argv[1], sPid, sTargetId, sRealId, sHp);
+		write(log_fd, logLine, sizeof(logLine));
+        write(fifoFd, &info, sizeof(Status));
+        close(fifoFd);
+	}
+	exit(0);
+	return 0;
+}		memset(sHp, 0, strlen(sHp)); sprintf(sHp, "%d", info.HP);
 			memset(sCurBattle, 0, strlen(sCurBattle)); sprintf(sCurBattle, "%c", info.current_battle_id);
 			memset(sEndFlag, 0, strlen(sEndFlag)); sprintf(sEndFlag, "%d", info.battle_ended_flag);
 			logTo(logLine, argv[1], sPid, sCurBattle, argv[2], sRealId, sHp, sCurBattle, sEndFlag);
