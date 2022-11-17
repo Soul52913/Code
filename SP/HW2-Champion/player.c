b50f07ea434a3b01e4abde0aa832e41b2f2a5496
Soul52913 <bingyi92@gmail.com>
2022-11-17 11:54:45 +0800

    a

--- SP/HW2-Champion/player.c
+++ SP/HW2-Champion/player.c
@@ -1,4 +1,4 @@
-//#include "status.h"
+#include "status.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,21 +6,6 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
-typedef enum {
-    FIRE,
-    GRASS,
-    WATER
-} Attribute;
-
-typedef struct {
-    int real_player_id; // ID of the Real Player, an integer from 0 - 7
-    int HP; // Healthy Point
-    int ATK; // Attack Power
-    Attribute attr; // Player's attribute
-    char current_battle_id; //current battle ID
-    int battle_ended_flag; // a flag for validate if the battle is the last round or not, 1 for yes, 0 for no
-} Status;
 
 #define PLAYER_PATH "./player_status.txt"
 
