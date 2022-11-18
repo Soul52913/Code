d4ec43247f813042dc6f02f2bd79e9943918eb62
Soul52913 <bingyi92@gmail.com>
2022-11-18 13:25:15 +0800

    a

--- SP/HW2-Champion/battle.c
+++ SP/HW2-Champion/battle.c
@@ -1,5 +1,80 @@
 #include "status.h"
-int main () {
-	//TODO
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main (int argc, char **argv) {
+	FILE * file_fd, * log_fd;
+	Status pone, ptwo;
+	char buf[512], bbuf[512];
+	int child[14][2] = {{-1, -1}, {-1, -1}, {14, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {0, 1}, {2, 3}, {4, 5}, {6, 7}, {10, -1}, {13, -1}, {8, 9}, {11, 12}};
+	char parent[14] = {'', 'A', 'A', 'B', 'B', 'C', 'D', 'D', 'E', 'E',, 'F', 'F', 'K', 'L'};
+	int child[14] = {7, };
+	Attribute[14] = {FIRE, GRASS, WATER, WATER, FIRE, FIRE, FIRE, GRASS, WATER, GRASS, GRASS, GRASS, FIRE, WATER}
+	int agentid[128];
+	char battleId = atoi(argv[1]);
+	int pPid = atoi(argv[2]);
+	pid_t pid = getpid(), cpid;
+	sprintf(buf, "log_battle%d.txt", battleId);
+	log_fd = fopen(buf, "a+");
+	int pone_pipeIN[2], pone_pipeOUT[2], ptwo_pipeIN[2], ptwo_pipeOUT[2];
+	if(pipe(pone_pipeIN) < 0)	return -1;
+	if(pipe(pone_pipeOUT) < 0)	return -1;
+	if(pipe(ptwo_pipeIN) < 0)	return -1;
+	if(pipe(ptwo_pipeOUT) < 0)	return -1;
+	for (int i = 0; i < 2; ++i){
+		if((cpid = fork()) == 0) {
+			pid_t ccpid;
+			if((ccpid = fork()) == 0) {
+				if (i == 0){
+					dup2(pone_pipeIN[0].fd, 1);
+					close(pone_pipeIN[1]);
+					dup2(0, pone_pipeOUT[1].fd);
+					close(pone_pipeOUT[0]);
+					if (child[battleId - 'A'][i] != -1){
+						sprintf(buf, "%c", battleId);
+						sprintf(bbuf, "%d", pid);
+						execlp("./battle", "battle", buf, bbuf, NULL);
+					}
+					else{
+						sprintf(buf, "%c", battleId);
+						sprintf(bbuf, "%d", pid);
+						execlp("./player", "player", buf, bbuf, NULL);
+					}
+				}
+				else{
+					dup2(ptwo_pipeIN[0].fd, 1);
+					close(ptwo_pipeIN[1]);
+					dup2(0, ptwo_pipeOUT[1].fd);
+					close(ptwo_pipeOUT[0]);
+					if (child[battleId - 'A'][i] != -1){
+						execlp("./battle", "battle", "A", "0", NULL);
+					}
+					else{
+						execlp("./player", "player", "A", "0", NULL);
+					}
+				}
+			}
+			else{
+				exit(0);
+			}
+		}
+		else if (cpid > 0){
+		}
+	}
+	fprintf(log_fd, "%c,%d pipe to %c,%d %d,%d,%c,%d\n", battleId, pid, targetId[playerId], pPid, pone.real_player_id, pone.HP, pone.current_battle_id, pone.battle_ended_flag);
+			write(1, &info, sizeof(Status));
+			read(0, &info, sizeof(Status));
+			fprintf(log_fd, "%d,%d pipe from %c,%d %d,%d,%c,%d\n", playerId, pid, targetId[playerId], pPid, info.real_player_id, info.HP, info.current_battle_id, info.battle_ended_flag);
+	read(pone_pipeIN[1].fd, &pone, sizeof(Status));
+	read(ptwo_pipeIN[1].fd, &ptwo, sizeof(Status));
+	while (pone.HP > 0 && ptwo.HP > 0){
+		
+	}
+	exit(0);
 	return 0;
 }
--- SP/HW2-Champion/player.c
+++ SP/HW2-Champion/player.c
@@ -9,33 +9,32 @@
 
 #define PLAYER_PATH "./player_status.txt"
 
-int fifoFd;
-FILE * file_fd, * log_fd;
-int fullHp, agentCheck;
-Status info;
-char buf[512];
-char targetId[20] = {'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'M', 'M', 'K', 'N', 'N', 'L', 'C'};
-int agentid[128];
-
 int main (int argc, char **argv) {
+	int fifoFd = 0;
+	FILE * file_fd, * log_fd;
+	int fullHp = 0;
+	Status info;
+	char buf[512];
+	char targetId[20] = {'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'M', 'M', 'K', 'N', 'N', 'L', 'C'};
+	int agentid[128] = {0};
 	int playerId = atoi(argv[1]);
 	int pPid = atoi(argv[2]);
 	agentid['G'] = 8, agentid['I'] = 9, agentid['D'] = 10, agentid['H'] = 11, agentid['J'] = 12;
 	agentid['E'] = 13, agentid['B'] = 14;
 	pid_t pid = getpid();
 	if (playerId > 7){
-		agentCheck = 1;
 		sprintf(buf, "player%d.fifo", playerId);
 		mkfifo(buf, 0700);
 		fifoFd = open(buf, O_RDONLY);
-		read(fifoFd, &info, sizeof(fifoFd));
+		read(fifoFd, &info, sizeof(Status));
+		sprintf(buf, "log_player%d.txt", info.real_player_id);
+		log_fd = fopen(buf, "a+");
 		fprintf(log_fd, "%d,%d fifo from %d %d,%d\n", playerId, pid, info.real_player_id, info.real_player_id, info.HP);
-        close(fifoFd);
 	}
 	else if (playerId <= 7 && playerId >= 0){
 		file_fd = fopen(PLAYER_PATH, "r");
 		char attrTemp[16];
-		for(int i = 0; i < playerId; ++i){
+		for(int i = 0; i <= playerId; ++i){
 			fscanf(file_fd, "%d %d %s %c %d\n", &info.HP, &info.ATK, attrTemp, &info.current_battle_id, &info.battle_ended_flag);
 		}
 		info.real_player_id = playerId;
@@ -45,11 +44,12 @@ int main (int argc, char **argv) {
 			info.attr = GRASS;
 		else if (strcmp(attrTemp, "WATER") == 0) 
 			info.attr = WATER;
+		fclose(file_fd);
+		sprintf(buf, "log_player%d.txt", info.real_player_id);
+		log_fd = fopen(buf, "a+");
 	}
-	sprintf(buf, "log_player%d.txt", info.real_player_id);
-	log_fd = fopen(buf, "a+");
 	fullHp = info.HP;
-	while (info.HP > 0) {
+	while (info.HP > 0 && info.current_battle_id != 'A') {
 		if (info.battle_ended_flag == 1){
 			info.HP += ((fullHp - info.HP) / 2);
 			info.battle_ended_flag = 0;
@@ -61,15 +61,15 @@ int main (int argc, char **argv) {
 			fprintf(log_fd, "%d,%d pipe from %c,%d %d,%d,%c,%d\n", playerId, pid, targetId[playerId], pPid, info.real_player_id, info.HP, info.current_battle_id, info.battle_ended_flag);
 		}
 	}
-	if (agentCheck == 0) {
-		info.HP = fullHp;
+	if (playerId <= 7 && playerId >= 0 && info.current_battle_id != 'A') {
+		info.HP = fullHp; info.battle_ended_flag = 0;
 		sprintf(buf, "player%d.fifo", agentid[info.current_battle_id]);
-		while(open(buf, O_WRONLY) != -1);
+		mkfifo(buf, 0700);
 		fifoFd = open(buf, O_WRONLY);
 		fprintf(log_fd, "%d,%d fifo to %d %d,%d\n", playerId, pid, agentid[info.current_battle_id], info.real_player_id, info.HP);
         write(fifoFd, &info, sizeof(Status));
-        close(fifoFd);
 	}
+	fclose(log_fd);
 	exit(0);
 	return 0;
 }
