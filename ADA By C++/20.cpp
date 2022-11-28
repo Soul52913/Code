5b3492dcb007b88d11af2ae3fd17efb255fbbc12
Soul52913 <bingyi92@gmail.com>
2022-11-28 18:26:58 +0800

    a

--- ADA By C++/20.cpp
+++ ADA By C++/20.cpp
@@ -11,31 +11,91 @@ struct Tree {
     int parent;
     long long c;
     long long d;
+    long long subTreeC;
+    int nodeNum;
     vector <int> child;
 };
 
 vector <Tree> forest;
 vector <int> roots;
-vector <vector <int>> dp;
+vector <int> seq;
+vector <vector <long long>> dp;
+
+void dfs (int index) {
+    long long totalC = forest[index].c;
+    int totalNode = 1;
+    for (int i = 0; i < forest[index].child.size(); ++i){
+        dfs(forest[index].child[i]);
+        totalC += forest[forest[index].child[i]].subTreeC;
+        totalNode += forest[forest[index].child[i]].nodeNum;
+    }
+    forest[index].subTreeC = totalC;
+    forest[index].nodeNum = totalNode;
+    seq.push_back(index);
+    return;
+}
+
+void dpING () {
+    for (int i = 0; i <= M; ++i) {
+        int index = seq[0];
+        if(i >= forest[index].c || i >= forest[index].d)
+            dp[index][i] = forest[index].c;
+    }
+    for(int i = 1; i < seq.size(); ++i) {
+        int index = seq[i];
+        int prev = seq[i - 1];
+        for (int j = 0; j <= M; ++j) {
+            dp[index][j] = dp[prev][j];
+            long long left = 0;
+            if(j >= forest[index].c) {
+                left = j - forest[index].c;
+                if (forest[index].c + dp[prev][left] > dp[index][j]){
+                    dp[index][j] = forest[index].c + dp[prev][left];
+                }
+            }
+            if(j >= forest[index].d) {
+                left = j - forest[index].d;
+                long long dpleft = 0;
+                if (i - forest[index].nodeNum >= 0)
+                    dpleft = dp[seq[i - forest[index].nodeNum]][left];
+                if (forest[index].subTreeC + dpleft > dp[index][j]){
+                    dp[index][j] = forest[index].subTreeC + dpleft;
+                }
+            }
+        }
+    }
+    return;
+}
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
     cin >> N >> M;
-	forest.resize(N);
-    vector <int> temp (N, 0);
-    for (int i = 0; i <= M; ++i){
+    forest.resize(N);
+    vector <long long> temp (M + 1, 0);
+    for (int i = 0; i < N; ++i)
         dp.push_back(temp);
-    }
-    Tree tree;
     for(int i = 0; i < M; ++i){
         cin >> forest[i].parent;
         if (forest[i].parent == -1)
             roots.push_back(i);
+        else
+            forest[forest[i].parent].child.push_back(i);
     }
     for(int i = 0; i < M; ++i){
         cin >> forest[i].c >> forest[i].d;
     }
-    
+    for (int i = 0; i < roots.size(); ++i){
+        dfs(roots[i]);
+    }
+    dpING();
+    //for (auto next : forest) cout << next.nodeNum;
+    for(int i = 0; i < seq.size(); ++i){
+        int index = seq[i];
+        for(int j = 0; j <= M; ++j){
+            cout << dp[index][j] << " ";
+        }cout << endl;
+    }
+    cout << dp[seq[(seq.size() - 1)]][M];
 	return 0;
 }
--- ADA By C++/21.cpp
+++ ADA By C++/21.cpp
@@ -1,102 +1,101 @@
-#include<bits/stdc++.h>
 #include <limits.h>
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-struct City {
-    int city;
-    long long dist;
-    long long cost;
-};
-
-struct Node {
-    long long dist;
-    long long cost;
-    int city;
-};
-
-struct cmp {
-    bool operator()(Node a, Node b) {
-        if (a.dist == b.dist)
-            return a.cost > b.cost;
-        else
-            return a.dist > b.dist; 
-    }
-};
-
-vector <vector <City>> points;
 int N {}, M {};
 
-long long prim(){
-	priority_queue <Node, vector <Node>, cmp> pq;
-	vector<long long> keyD(N, INT64_MAX);
-	vector<long long> keyMaxD(N, INT64_MIN);
-	vector<long long> keyC(N, INT64_MAX);
-	vector<int> parent(N, -1);
-	vector<bool> visited(N, false);
-    Node node = {0, 0, 0};
-	pq.push(node);
-	keyD[0] = 0;
-    keyMaxD[0] = 0;
-    keyC[0] = 0;
-	while (!pq.empty())	{
-		int c = pq.top().city;
-        long long cc = pq.top().cost;
-        //cout << c << endl;
-		pq.pop();
-		if(visited[c])
-			continue;
-		visited[c] = true; //for(auto next : points[c])
-        keyC[c] = cc;
-		for (int i = 0; i < points[c].size(); ++i) {
-			int toC {points[c][i].city};
-			long long dist {points[c][i].dist};
-            long long cost {points[c][i].cost};
-			if (!(visited[toC]) && keyD[toC] >= dist){
-                if (dist > keyMaxD[c]){
-                    keyMaxD[toC] = dist;
-                    keyC[toC] = cost;
+struct Tree {
+    int parent;
+    long long c;
+    long long d;
+    long long subTreeC;
+    int nodeNum;
+    vector <int> child;
+};
+
+vector <Tree> forest;
+vector <int> roots;
+vector <int> seq;
+vector <vector <long long>> dp;
+
+void dfs (int index) {
+    long long totalC = forest[index].c;
+    int totalNode = 1;
+    for (int i = 0; i < forest[index].child.size(); ++i){
+        dfs(forest[index].child[i]);
+        totalC += forest[forest[index].child[i]].subTreeC;
+        totalNode += forest[forest[index].child[i]].nodeNum;
+    }
+    forest[index].subTreeC = totalC;
+    forest[index].nodeNum = totalNode;
+    seq.push_back(index);
+    return;
+}
+
+void dpING () {
+    for (int i = 0; i <= M; ++i) {
+        int index = seq[0];
+        if(i >= forest[index].c || i >= forest[index].d)
+            dp[index][i] = forest[index].c;
+    }
+    for(int i = 1; i < seq.size(); ++i) {
+        int index = seq[i];
+        int prev = seq[i - 1];
+        for (int j = 0; j <= M; ++j) {
+            dp[index][j] = dp[prev][j];
+            long long left = 0;
+            if(j >= forest[index].c) {
+                left = j - forest[index].c;
+                if (forest[index].c + dp[prev][left] > dp[index][j]){
+                    dp[index][j] = forest[index].c + dp[prev][left];
                 }
-                else if (dist == keyMaxD[c]) {
-                    keyMaxD[toC] = dist;
-                    keyC[toC] = cost + keyC[c];
+            }
+            if(j >= forest[index].d) {
+                left = j - forest[index].d;
+                long long dpleft = 0;
+                if (i - forest[index].nodeNum >= 0)
+                    dpleft = dp[seq[i - forest[index].nodeNum]][left];
+                if (forest[index].subTreeC + dpleft > dp[index][j]){
+                    dp[index][j] = forest[index].subTreeC + dpleft;
                 }
-                else {
-                    keyMaxD[toC] = keyMaxD[c];
-                    keyC[toC] = keyC[c];
-                }
-				keyD[toC] = dist;
-                node.city = toC, node.dist = keyD[toC], node.cost = keyC[toC];
-                //cout << node.city << " " << node.dist << endl;
-				pq.push(node);
-				parent[toC] = c;
-			}
-            //for (int i = 0; i < N; ++i){cout << keyD[i] << " ";}
-		    //cout << "a" << endl;
-		}//cout << "b" << endl;
-	}
-    long long min = {};
-	for (int i = 0; i < N; ++i)
-        min += keyC[i];
-    return min;
+            }
+        }
+    }
+    return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-	int temp {};
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
     cin >> N >> M;
-    points.resize(N + 1);
-    City input;
+    forest.resize(N);
+    vector <long long> temp (M + 1, 0);
+    for (int i = 0; i < N; ++i)
+        dp.push_back(temp);
     for(int i = 0; i < M; ++i){
-        cin >> temp >> input.city >> input.dist >> input.cost;
-        points[temp].push_back(input);
-        int tempp = temp;
-        temp = input.city, input.city = tempp;
-        points[temp].push_back(input);
+        cin >> forest[i].parent;
+        if (forest[i].parent == -1)
+            roots.push_back(i);
+        else
+            forest[forest[i].parent].child.push_back(i);
     }
-    cout << prim();
+    for(int i = 0; i < M; ++i){
+        cin >> forest[i].c >> forest[i].d;
+    }
+    for (int i = 0; i < roots.size(); ++i){
+        dfs(roots[i]);
+    }
+    dpING();
+    //for (auto next : forest) cout << next.nodeNum;
+    for(int i = 0; i < seq.size(); ++i){
+        int index = seq[i];
+        for(int j = 0; j <= M; ++j){
+            cout << dp[index][j] << " ";
+        }cout << endl;
+    }
+    cout << dp[seq[(seq.size() - 1)]][M];
 	return 0;
 }
